53c2862da0ea9111008cad18f109159c5263b128
AJAY0993 <ajaymishra5277919886@gmail.com>
2024-04-03 23:44:13 +0530

    added security features

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors')
-const logger = require('morgan')
-const videoRouter = require('./routes/videoRoutes');
-const userRouter = require('./routes/userRoutes.js');
-const playlistRouter = require('./routes/playlistRoutes.js')
-const commentRouter = require('./routes/commentsRoutes.js')
-const AppError = require('./utils/appError.js');
-const errorController = require('./controllers/errorController.js');
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoSanitize = require("express-mongo-sanitize")
+const helmet = require("helmet")
+const videoRouter = require("./routes/videoRoutes")
+const userRouter = require("./routes/userRoutes.js")
+const playlistRouter = require("./routes/playlistRoutes.js")
+const commentRouter = require("./routes/commentsRoutes.js")
+const AppError = require("./utils/appError.js")
+const errorController = require("./controllers/errorController.js")
 
-const app = express();
+const app = express()
 
 // 1) MIDDLEWARES
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
 }
-
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(mongoSanitize())
+app.use(helmet())
 app.use(cors())
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    console.log(req.originalUrl)
-    next();
-});
-
+  req.requestTime = new Date().toISOString()
+  console.log(req.originalUrl)
+  next()
+})
 
 // 3) ROUTES
-app.use('/api/v1/videos', videoRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/playlists', playlistRouter);
-app.use('/api/v1/comments', commentRouter);
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/comments", commentRouter)
 
 console.log(process.env.NODE_ENV)
-app.all('*', (req, res, next) => {
-    const err = new AppError(`Cant't find ${req.originalUrl} on this server`, 404)
-    next(err)
+app.all("*", (req, res, next) => {
+  const err = new AppError(`Cant't find ${req.originalUrl} on this server`, 404)
+  next(err)
 })
 
 app.use(errorController)
 
-
-module.exports = app;
-
+module.exports = app
